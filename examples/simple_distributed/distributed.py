95fb3579467e27081c84e533f22f11336e6368e6
Dmitriy Fedorov <dmifedorov@tracto.ai>
2024-05-15 15:37:31 +0200

    make torchesaurus a python lib

--- examples/simple_distributed/distributed.py
+++ examples/simple_distributed/distributed.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
 from torchesaurus.run import run
 from torchesaurus.job_client import JobClient
 
--- examples/torch_mnist/torch_mnist.py
+++ examples/torch_mnist/torch_mnist.py
@@ -1,9 +1,3 @@
-from __future__ import print_function
-
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
 import argparse
 import torch
 import torch.nn as nn

ce55aafd8e3f9a9d3e5b6ac295149789c0c51f8b
Maksim Zuev <chiffa@tracto.ai>
2024-07-10 22:35:01 +0200

    Fix defaults, add tests

--- tractorun/cli/tractorun_runner.py
+++ tractorun/cli/tractorun_runner.py
@@ -23,13 +23,10 @@ from tractorun.resources import Resources
 from tractorun.run import run_script
 
 
-CONFIG_PATH_DEFAULT = None
-NNODES_DEFAULT = 1
-NPROC_PER_NODE_DEFAULT = 0
-NGPU_PER_PROC_DEFAULT = 0
+MESH_NODE_COUNT_DEFAULT = 1
+MESH_PROCESS_PER_NODE_DEFAULT = 1
+MESH_GPU_PER_PROCESS_DEFAULT = 0
 LOCAL_DEFAULT = False
-CPU_LIMIT_DEFAULT = None
-MEM_LIMIT_DEFAULT = None
 
 
 @attr.define(kw_only=True, slots=True, auto_attribs=True)
@@ -46,25 +43,13 @@ class ResourcesConfig:
     memory_limit: Optional[int] = attr.ib(default=None)
 
 
-@attr.define(kw_only=True, slots=True, auto_attribs=True)
-class EffectiveMeshConfig:
-    node_count: int
-    process_per_node: int
-    gpu_per_process: int
-    pool_trees: Optional[List[str]]
-
-
-@attr.define(kw_only=True, slots=True)
-class EffectiveResourcesConfig:
-    cpu_limit: float
-    memory_limit: int
-
-
 _T = TypeVar("_T")
 
 
 @attr.define(kw_only=True, slots=True, auto_attribs=True)
 class Config:
+    """yaml config representation"""
+
     yt_path: Optional[str] = attr.field(default=None)
     docker_image: Optional[str] = attr.field(default=None)
     user_config: Optional[Dict[str, Any]] = attr.field(default=None)
@@ -85,8 +70,22 @@ class Config:
 
 
 @attr.define(kw_only=True, slots=True, auto_attribs=True)
+class EffectiveMeshConfig:
+    node_count: int
+    process_per_node: int
+    gpu_per_process: int
+    pool_trees: Optional[List[str]]
+
+
+@attr.define(kw_only=True, slots=True)
+class EffectiveResourcesConfig:
+    cpu_limit: Optional[float]
+    memory_limit: Optional[int]
+
+
+@attr.define(kw_only=True, slots=True, auto_attribs=True)
 class EffectiveConfig:
-    yt_path: str = attr.field(default=None)
+    yt_path: str
     docker_image: str
     user_config: Optional[Dict[str, Any]]
     yt_operation_spec: Optional[Dict[str, Any]]
@@ -100,8 +99,10 @@ class EffectiveConfig:
 
     @classmethod
     def configure(cls, args: Dict[str, Any], config: Config) -> "EffectiveConfig":
-        def _choose_value(args_value: _T, config_value: _T) -> _T:
+        def _choose_value(args_value: _T, config_value: _T, default: Optional[_T] = None) -> _T:
             result = args_value if args_value is not None else config_value
+            if result is None and default is not None:
+                return default
             return result
 
         user_config = json.loads(args["user_config"]) if args["user_config"] is not None else None
@@ -123,21 +124,24 @@ class EffectiveConfig:
             user_config=_choose_value(args_value=user_config, config_value=user_config),
             yt_operation_spec=_choose_value(args_value=yt_operation_spec, config_value=yt_operation_spec),
             yt_task_spec=_choose_value(args_value=yt_task_spec, config_value=yt_task_spec),
-            local=_choose_value(args_value=args["local"], config_value=config.local),
+            local=_choose_value(args_value=args["local"], config_value=config.local, default=LOCAL_DEFAULT),
             bind=bind,
             command=command,
             mesh=EffectiveMeshConfig(
                 node_count=_choose_value(
                     args_value=args["mesh.node_count"],
                     config_value=config.mesh.node_count,
+                    default=MESH_NODE_COUNT_DEFAULT,
                 ),
                 process_per_node=_choose_value(
                     args_value=args["mesh.process_per_node"],
                     config_value=config.mesh.process_per_node,
+                    default=MESH_PROCESS_PER_NODE_DEFAULT,
                 ),
                 gpu_per_process=_choose_value(
                     args_value=args["mesh.gpu_per_process"],
                     config_value=config.mesh.gpu_per_process,
+                    default=MESH_GPU_PER_PROCESS_DEFAULT,
                 ),
                 pool_trees=_choose_value(
                     args_value=args["mesh.pool_trees"],
@@ -159,25 +163,29 @@ class EffectiveConfig:
 
 
 def main() -> None:
+    # Defaults shouldn't be set in argparse
+    # because it interferes with merging with configs.
+    # We don't want to use config values as defaults too.
     parser = argparse.ArgumentParser(
         description="Tractorun",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("--yt-path", help="YT workdir", type=str)
-    parser.add_argument("--run-config-path", type=str, help="path to tractorun config", default=CONFIG_PATH_DEFAULT)
-    parser.add_argument("--docker-image", type=str, help="docker image name", default=DEFAULT_DOCKER_IMAGE)
-    parser.add_argument("--mesh.node-count", type=int, help="mesh node count", default=NNODES_DEFAULT)
+    parser.add_argument("--run-config-path", type=str, help="path to tractorun config")
+    parser.add_argument("--docker-image", type=str, help=f"docker image name. Default: {DEFAULT_DOCKER_IMAGE}")
+    parser.add_argument("--mesh.node-count", type=int, help=f"mesh node count. Default: {MESH_NODE_COUNT_DEFAULT}")
     parser.add_argument(
-        "--mesh.process-per-node", type=int, help="mesh process per node", default=NPROC_PER_NODE_DEFAULT
+        "--mesh.process-per-node", type=int, help=f"mesh process per node. Default: {MESH_PROCESS_PER_NODE_DEFAULT}"
     )
-    parser.add_argument("--mesh.gpu_per_process", type=int, help="mesh gpu per process", default=NGPU_PER_PROC_DEFAULT)
+    parser.add_argument(
+        "--mesh.gpu-per-process", type=int, help=f"mesh gpu per process. Default: {MESH_GPU_PER_PROCESS_DEFAULT}"
+    )
     parser.add_argument("--mesh.pool-trees", help="mesh pool trees", action="append")
-    parser.add_argument("--resources.cpu-limit", type=int, help="cpu limit", default=CPU_LIMIT_DEFAULT)
-    parser.add_argument("--resources.mem-limit", type=int, help="mem limit", default=MEM_LIMIT_DEFAULT)
+    parser.add_argument("--resources.cpu-limit", type=int, help="cpu limit")
+    parser.add_argument("--resources.mem-limit", type=int, help="mem limit")
     parser.add_argument("--user-config", type=str, help="json config that will be passed to the jobs")
     parser.add_argument("--yt-operation-spec", help="yt operation spec", type=str)
     parser.add_argument("--yt-task-spec", help="yt task spec", type=str)
-    parser.add_argument("--local", type=bool, help="enable local run mode", default=LOCAL_DEFAULT)
+    parser.add_argument("--local", type=bool, help=f"enable local run mode. Default {LOCAL_DEFAULT}")
     parser.add_argument(
         "--bind",
         type=str,

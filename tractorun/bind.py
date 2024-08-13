4e2ac67ad9e3c23e8ed1f83bf7a1df6a37158496
Maksim Zuev <chiffa@tracto.ai>
2024-08-13 22:41:45 +0200

    NYT-627: fix mypy

--- tractorun/bind.py
+++ tractorun/bind.py
@@ -9,11 +9,16 @@ import zipfile
 import attrs
 
 
+def _to_abs_path(path: str) -> str:
+    # mypy workaround
+    return os.path.abspath(path)
+
+
 @attrs.define(kw_only=True, slots=True)
 class BindLocal:
     # TODO: just use pathlib
-    source: str = attrs.field(converter=lambda x: os.path.abspath(x))
-    destination: str = attrs.field(converter=lambda x: os.path.abspath(x))
+    source: str = attrs.field(converter=_to_abs_path)
+    destination: str = attrs.field(converter=_to_abs_path)
 
 
 @attrs.define(kw_only=True, slots=True, auto_attribs=True)

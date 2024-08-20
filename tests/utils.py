bf1afbc6914a486fd8bf134bfed091f6f71d40bf
Max Zuev <chiffa@tracto.ai>
2024-08-20 15:38:42 +0200

    NYT-639: add yaml to dependencies (#49)

--- tests/utils.py
+++ tests/utils.py
@@ -16,7 +16,7 @@ import yaml
 from yt import wrapper as yt
 
 
-DOCKER_IMAGE: str = "cr.ai.nebius.cloud/crnf2coti090683j5ssi/tractorun/torchesaurus_tests:2024-06-17-15-21-24"
+DOCKER_IMAGE: str = "cr.ai.nebius.cloud/crnf2coti090683j5ssi/tractorun/torchesaurus_tests:2024-08-20-15-10-52"
 
 
 def get_data_path(filename: str) -> str:

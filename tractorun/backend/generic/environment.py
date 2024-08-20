7b8e088c7f1be43d680df3a99a3df505d7390fb3
Maksim Zuev <chiffa@tracto.ai>
2024-08-20 22:50:40 +0200

    NYT-641: fix all section

--- tractorun/backend/generic/environment.py
+++ tractorun/backend/generic/environment.py
@@ -3,7 +3,7 @@ from tractorun.private.closet import Closet as _Closet
 from tractorun.private.environment import prepare_environment as _common_prepare_environment
 
 
-__all__ = ["EnvironmentBase"]
+__all__ = ["Environment"]
 
 
 class Environment(EnvironmentBase):

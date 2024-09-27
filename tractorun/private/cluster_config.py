6814fb2eb88db69aa73346d82fb8759f2d49be39
Maksim Zuev <chiffa@tracto.ai>
2024-09-28 00:48:45 +0200

    NYT-708: make cluster config optional

--- tractorun/private/cluster_config.py
+++ tractorun/private/cluster_config.py
@@ -4,10 +4,15 @@ import yt.wrapper as yt
 
 @attrs.define(kw_only=True, slots=True, auto_attribs=True)
 class TractorunClusterConfig:
-    cypress_link_template: str
+    cypress_link_template: str | None
 
     @staticmethod
-    def load_from_yt(yt_client: yt.YtClient, path: str) -> "TractorunClusterConfig":
+    def load_from_yt(yt_client: yt.YtClient, path: str, fail_if_not_exist: bool = False) -> "TractorunClusterConfig":
+        if not fail_if_not_exist and not yt_client.exists(path):
+            return TractorunClusterConfig(
+                cypress_link_template=None,
+            )
+
         config = yt_client.get(path)
 
         return TractorunClusterConfig(

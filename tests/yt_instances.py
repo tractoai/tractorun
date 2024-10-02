e9d0c881be221da04bffe2541c8ae8e48bf7ff74
Max Zuev <chiffa@tracto.ai>
2024-10-02 19:07:27 +0200

    Disable backoff retries for yt in test container (#70)

--- tests/yt_instances.py
+++ tests/yt_instances.py
@@ -2,11 +2,13 @@ from abc import (
     ABC,
     abstractmethod,
 )
+from datetime import timedelta
 from typing import Any
 
 import attrs
 import testcontainers_yt_local.container
 from yt.wrapper.client import YtClient
+from yt.wrapper.default_config import retries_config
 
 
 class YtInstance(ABC):
@@ -44,4 +46,17 @@ class YtInstanceTestContainers(YtInstance):
         self.yt_container.stop()
 
     def get_client(self) -> YtClient:
-        return self.yt_container.get_client()
+        return self.yt_container.get_client(
+            config={
+                "proxy": {
+                    "retries": retries_config(
+                        enable=True,
+                        total_timeout=timedelta(minutes=5),
+                        backoff={
+                            "policy": "constant_time",
+                            "constant_time": 5,
+                        },
+                    ),
+                },
+            },
+        )

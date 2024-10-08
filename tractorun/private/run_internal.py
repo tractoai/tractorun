1c89097f0d9def7027a5f7c1ad29d976c3de9fdd
Dmitriy Fedorov <dmifedorov@tracto.ai>
2024-10-08 18:58:05 +0200

    debug

--- tractorun/private/run_internal.py
+++ tractorun/private/run_internal.py
@@ -273,7 +273,8 @@ def run_tracto(
     yt_client.config["pickling"]["ignore_system_modules"] = False if attach_external_libs else True
 
     # we store it explicitly since locally it could be read from ~/.yt/token
-    yt_client.config["token"] = yt.http_helpers.get_token(client=yt_client)
+    print("DEBUG", yt_client.config)
+    # yt_client.config["token"] = yt.http_helpers.get_token(client=yt_client)
 
     yt_client_config = yt.config.get_config(yt_client)
     yt_client_config_pickled = base64.b64encode(pickle.dumps(yt_client_config)).decode("utf-8")

7f06f84ba0ad7e4d92a0ad3fec3d9d0170e8e67d
Max Zuev <chiffa@tracto.ai>
2024-10-30 18:51:58 +0100

    Hack env set up for torch (like for jax) (#121)

--- tractorun/backend/tractorch/environment.py
+++ tractorun/backend/tractorch/environment.py
@@ -1,3 +1,7 @@
+import socket
+import sys
+from urllib.parse import urlparse
+
 import torch
 import torch.distributed
 
@@ -19,6 +23,14 @@ class Environment(EnvironmentBase):
             device_index = closet.coordinator.get_process_index()
             assert device_index < torch.cuda.device_count()
             torch.cuda.set_device(closet.coordinator.get_process_index())
+
+        coordinator_address = closet.coordinator.get_primary_endpoint()
+        print("old coordinator address: {}".format(coordinator_address), file=sys.stderr)
+        parsed_coordinator_address = urlparse(f"schema://{coordinator_address}")
+        # because of overlay problems
+        if parsed_coordinator_address.hostname == socket.gethostname():
+            coordinator_address = f"127.0.0.1:{parsed_coordinator_address.port}"
+            print("new coordinator address: {}".format(coordinator_address), file=sys.stderr)
 
         backend = "gloo" if closet.mesh.gpu_per_process == 0 else "nccl"
         torch.distributed.init_process_group(

53b28aa358ee4b5abc2497ac375427c86278e5e4
Maksim Zuev <chiffa@tracto.ai>
2024-11-20 15:17:59 +0100

    rename example scripts

--- examples/pytorch/lightning_mnist_ddp_script/run.sh
+++ examples/pytorch/lightning_mnist_ddp_script/run.sh
@@ -3,9 +3,9 @@ tractorun \
     --mesh.process-per-node 4 \
     --mesh.gpu-per-process 0 \
     --resources.memory-limit 8076021002 \
-    --yt-path //tmp/$USER/$RANDOM \
-    --user-config '{"dataset_path": "//home/samples/mnist-torch-train"}' \
-    --bind-local './script.py:/script.py' \
+    --yt-path ${SAMPLE_YT_PATH:-//tmp/$USER/$RANDOM} \
+    --user-config "{'dataset_path': ${SAMPLE_YT_PATH:"//home/samples/mnist-torch-train"}}" \
+    --bind-local './lightning_mnist_ddp_script.py:/lightning_mnist_ddp_script.py' \
     --bind-local-lib '../../../tractorun' \
     --docker-image $DOCKER_IMAGE \
-    python3 /script.py
+    python3 /lightning_mnist_ddp_script.py

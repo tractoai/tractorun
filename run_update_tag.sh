9cbde33e131ee784536133ef5b64564d4327e6cc
Maksim Zuev <chiffa@tracto.ai>
2024-12-20 21:16:33 +0100

    NYT-814: update tag in readme too

--- run_update_tag.sh
+++ run_update_tag.sh
@@ -9,6 +9,6 @@ fi
 
 LC_ALL=C.UTF-8 find . -type f -name "*.sh" -exec sed -i '' -E "s/^(_CURRENT_DOCKER_TAG=).*/\1\"$NEW_TAG\"/" {} +
 LC_ALL=C.UTF-8 find . -type f -name "*.py" -exec sed -i '' -E "s/^(_CURRENT_DOCKER_TAG = ).*/\1\"$NEW_TAG\"/" {} +
-#LC_ALL=C.UTF-8 sed -i '' -E "s|--docker-image ghcr.io/tractoai/tractorun-examples-runtime:[^ ]*|--docker-image ghcr.io/tractoai/tractorun-examples-runtime:$NEW_TAG|" ./README.md
+LC_ALL=C.UTF-8 sed -i '' -E "s|--docker-image ghcr.io/tractoai/tractorun-examples-runtime:[^ ]*|--docker-image ghcr.io/tractoai/tractorun-examples-runtime:$NEW_TAG|" ./README.md
 
 echo "Tag updated successfully in all files."

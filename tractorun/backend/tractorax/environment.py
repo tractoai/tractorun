ec639faceab8c4379f48f6bcef3e0738b84e4b84
Maksim Zuev <chiffa@tracto.ai>
2025-02-14 19:31:30 +0100

    Fix primary endpoint host due to nebius network configuration

--- tractorun/backend/tractorax/environment.py
+++ tractorun/backend/tractorax/environment.py
@@ -1,7 +1,3 @@
-import socket
-import sys
-from urllib.parse import urlparse
-
 import jax
 
 from tractorun.base_backend import EnvironmentBase
@@ -19,16 +15,8 @@ class Environment(EnvironmentBase):
             first_device_index = process_id * closet.mesh.gpu_per_process
             local_device_ids = list(range(first_device_index, first_device_index + closet.mesh.gpu_per_process))
 
-        coordinator_address = closet.coordinator.get_primary_endpoint()
-        print("old coordinator address: {}".format(coordinator_address), file=sys.stderr)
-        parsed_coordinator_address = urlparse(f"schema://{coordinator_address}")
-        # because of overlay problems
-        if parsed_coordinator_address.hostname == socket.gethostname():
-            coordinator_address = f"127.0.0.1:{parsed_coordinator_address.port}"
-            print("new coordinator address: {}".format(coordinator_address), file=sys.stderr)
-
         jax.distributed.initialize(
-            coordinator_address=coordinator_address,
+            coordinator_address=closet.coordinator.get_primary_endpoint(),
             num_processes=closet.coordinator.get_total_peer_count(),
             process_id=closet.coordinator.get_self_index(),
             local_device_ids=local_device_ids,

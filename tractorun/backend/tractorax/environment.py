c0a3d683b002891c2f77aa225db4eb527a398ad7
Maksim Zuev <chiffa@tracto.ai>
2025-02-17 13:58:41 +0100

    Revert "Fix primary endpoint host due to nebius network configuration (#187)"
    This reverts commit 0c8cf7715b1f39c41f1333013971d2cf539b0e10.

--- tractorun/backend/tractorax/environment.py
+++ tractorun/backend/tractorax/environment.py
@@ -1,3 +1,7 @@
+import socket
+import sys
+from urllib.parse import urlparse
+
 import jax
 
 from tractorun.base_backend import EnvironmentBase
@@ -15,8 +19,16 @@ class Environment(EnvironmentBase):
             first_device_index = process_id * closet.mesh.gpu_per_process
             local_device_ids = list(range(first_device_index, first_device_index + closet.mesh.gpu_per_process))
 
+        coordinator_address = closet.coordinator.get_primary_endpoint()
+        print("old coordinator address: {}".format(coordinator_address), file=sys.stderr)
+        parsed_coordinator_address = urlparse(f"schema://{coordinator_address}")
+        # because of overlay problems
+        if parsed_coordinator_address.hostname == socket.gethostname():
+            coordinator_address = f"127.0.0.1:{parsed_coordinator_address.port}"
+            print("new coordinator address: {}".format(coordinator_address), file=sys.stderr)
+
         jax.distributed.initialize(
-            coordinator_address=closet.coordinator.get_primary_endpoint(),
+            coordinator_address=coordinator_address,
             num_processes=closet.coordinator.get_total_peer_count(),
             process_id=closet.coordinator.get_self_index(),
             local_device_ids=local_device_ids,

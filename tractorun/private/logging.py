86ccde4a4db74ce170bf4ff9e08c996d888d8c50
Maksim Zuev <chiffa@tracto.ai>
2025-02-19 01:33:45 +0100

    Fix doc and yt log formatter

--- tractorun/private/logging.py
+++ tractorun/private/logging.py
@@ -1,18 +1,25 @@
 import logging
 import os
 
+from yt.logger import SimpleColorizedStreamHandler
+
 from tractorun.exception import TractorunConfigurationError
 
 
 def setup_logging() -> str | None:
     log_level = os.environ.get("YT_LOG_LEVEL")
     if log_level:
-        logger = logging.getLogger("tractorun")
-        logger.setLevel(log_level)
-        if not logger.handlers:
-            handler = logging.StreamHandler()
-            handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-            logger.addHandler(handler)
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+
+        tracto_logger = logging.getLogger("tractorun")
+        tracto_logger.setLevel(log_level)
+        if not tracto_logger.handlers:
+            tracto_logger.addHandler(handler)
+        yt_logger = logging.getLogger("Yt")
+        if len(yt_logger.handlers) == 1 and isinstance(yt_logger.handlers[0], SimpleColorizedStreamHandler):
+            yt_logger.handlers.clear()
+            yt_logger.addHandler(handler)
     return log_level
 
 
